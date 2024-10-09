43fc7e7bc69f7112ec5c72a91ff7ba5d1132642b
ysw711 <sinwei0711@gmail.com>
2024-10-09 22:40:31 +0800

    StorytellingGame page

---
+++ frontend/src/routes/StorytellingGamePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../containers/navbar/Navbar';
+import Footer from '../containers/footer/Footer';
+import StorytellingGame from './childUser/StorytellingGame';
+
+const StorytellingGamePage = () => {
+  return (
+    <>
+      <Navbar />
+      <StorytellingGame />  
+      <Footer />
+    </>
+  );
+};
+
+export default StorytellingGamePage;
