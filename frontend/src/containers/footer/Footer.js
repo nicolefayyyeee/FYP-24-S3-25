9b0be575533ba96b56170e4d67238772867e1adf
faye <nicolefayewong@gmail.com>
2024-10-20 17:58:00 +0800

    edit footer

--- frontend/src/containers/footer/Footer.js
+++ frontend/src/containers/footer/Footer.js
@@ -4,7 +4,7 @@ import '../footer/Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>“Empowering creativity, one moment at a time.”</p>
+      <p className="footer-quote">Empowering creativity, one moment at a time.</p>
       <p>© 2024 SeeSay Moments. All rights reserved.</p>
     </footer>
   );
